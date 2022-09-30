e8c814f7bafa4695e7ee96f0e7ebbecaa682eed6
silvia <villalobos.veas@gmail.com>
2022-09-30 09:20:32 -0300

    refactorizacion point

--- src/01-point/point.py
+++ src/01-point/point.py
@@ -1,22 +1,27 @@
+import math
+
 class Point:
     def __init__(self,x = 0,y = 0):
-        self.x = x
-        self.y = y
-
+        self.move(x,y)
+        
     def move(self, x, y):
         self.x = x
         self.y = y
 
-    def reset(self, x = 0, y = 0):
-        self.x = x
-        self.y = y
-
+    def reset(self):
+        self.move(0,0)
+        
     def distance_to(self,other_point):
         distance_x = self.x - other_point.x
         distance_y = self.y - other_point.y
 
-        distance = (distance_x**2 + distance_y**2)**(1/2)
+        distance = math.sqrt(distance_x**2 + distance_y**2)
+        
         return distance
+
+    
+
+    
         
 
 
